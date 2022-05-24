b72eda15e7cbd9985ba751f417fb0b33c391a922
Дима Киселёв <iamnotkisell@gmail.com>
2022-05-25 01:36:17 +0300

    чет неясно

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -16,9 +16,29 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
+from random import randint
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
+
 # TODO здесь ваш код
+def one_day():
+    error_n = randint(0, 7)
+    dice = randint(1, 14)
+    karma = randint(1, 7)
+    errors = ['IamGodError', 'DrunkError', 'CarCrashError', 'GluttonyError', 'DepressionError', 'SuicideError']
+    if dice == 1:
+        raise BaseException(errors[error_n])
+    return karma
+
 
 # https://goo.gl/JnsDqu
+current_karma = 0
+exceptions = []
+while current_karma <= ENLIGHTENMENT_CARMA_LEVEL:
+    try:
+        current_karma += one_day()
+    except BaseException as exc:
+        exceptions.append(str(exc))
+
+print(exceptions)
